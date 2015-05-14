22a1f9e657d43fdd18fd95df4c3239e661baf400
Matthew Williams <mwilliamsnov6@gmail.com>
2015-05-13 20:33:35 -0700

    I see your true colors shining through

--- script.js
+++ script.js
@@ -10,38 +10,59 @@ var randomColor = function () {
 	return start;
 }
 
-var black = function (){
+var colorFader = function(y) {
+	var start = "#3E3E"
+	return start + y + "5"
+
+
+	//return 3E3E + B5
+	// 3E3EB5
+}
+
+var colorFader2 = function(y) {
+	var start = "#63AD"
+	return start + y + "2"
+
+}
+
+console.log(colorFader(1))
+
+var black = function (x){
 	var div = document.createElement('div')
 	div.style.height = "11.1%"
 	div.style.width = "11.1%"
 	div.style.float = "left"
 	div.style.paddingBottom = "11.1%"
-	div.style.backgroundColor = randomColor()
+	div.style.backgroundColor = colorFader(x)
 	document.body.appendChild(div)
 }
 
-var red = function (){
+var red = function (y){
 	var div = document.createElement('div')
 	div.style.height = "11.1%"
 	div.style.width = "11.1%"
 	div.style.float = "left"
 	div.style.paddingBottom = "11.1%"
-	div.style.backgroundColor = randomColor()
+	div.style.backgroundColor = colorFader2(y)
 	document.body.appendChild(div)
 }
 
+var oneLine = function(k) {
+	arr = [];
+	for ( i = 0 ; i < 9 ; i++) {
+		arr.push (red(k))
+		arr.push (black(k))
+	}
+}
 
 
-//console.log(randomColor())
-
-var lineOne = function() {
-	for (i = 0 ; i < 180 ; i++) {
-		arr = [];
-			arr.push (red())
-			arr.push (black())
+var wholeEvent = function() {
+	arr = [];
+	for (za = 0 ; za < 9 ; za++) {
+		arr.push(oneLine(za))	
 	}
 	return arr
 }
 
-lineOne();
+wholeEvent();
 
